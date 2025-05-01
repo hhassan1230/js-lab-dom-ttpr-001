10d9ae3d6e701e3a99ccf4a23b337f168a90372f
Phyu Phyu Htar <phyuphyuhtar@gmail.com>
2025-05-01 12:04:28 -0400

    Implement dark mode toggle

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 function toggleDarkMode() {
     // This function should toggle the dark mode class on the body
     // comment out the alert below and replace it with your code
-    alert("change color in this function");
+   // alert("change color in this function");
   /*
     TODO: Replace the alert above with code that toggles
     the 'dark' class on <body>.
   */
+    document.body.classList.toggle('dark');
 }
 
 document
