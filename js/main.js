217d0c4fb839f344b3da3ff70ebbd13d100a0ea9
Tao Su <96sutao@gmail.com>
2025-05-01 22:57:47 -0400

    Improved the dark mode's text change, prevent text rest incorrectly from refreshing the page.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
+const savedMode = localStorage.getItem("mode");
+const body = document.body;
+
+if (savedMode) {
+    body.classList.add(savedMode);
+} else {
+    body.classList.add("light");
+}
+const toggleButton = document.getElementById("toggle-btn");
+
+toggleButton.innerHTML = savedMode === "dark"
+    ? "🌤️ Toggle Light Mode"
+    : "🌙 Toggle Dark Mode";
+
 function toggleDarkMode() {
     // This function should toggle the dark mode class on the body
     // comment out the alert below and replace it with your code
-    document.body.classList.toggle("dark");
-  /*
-    TODO: Replace the alert above with code that toggles
-    the 'dark' class on <body>.
-  */
+    // document.body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        body.classList.replace("dark", "light");
+        localStorage.setItem("mode", "light");
+        toggleButton.innerHTML = "🌙 Toggle Dark Mode";
+    } else {
+        body.classList.replace("light", "dark");
+        localStorage.setItem("mode", "dark");
+        toggleButton.innerHTML = "🌤️ Toggle Light Mode"
+    }
+
+    /*
+      TODO: Replace the alert above with code that toggles
+      the 'dark' class on <body>.
+    */
+
+
 }
 
 document
-  .getElementById("toggle-btn")
-  .addEventListener("click", toggleDarkMode);
+    .getElementById("toggle-btn")
+    .addEventListener("click", toggleDarkMode);
